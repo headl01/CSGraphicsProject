2638e9a477b63e607451fc1974e80717baa4798a
Pete Willemsen <willemsn@d.umn.edu>
2026-01-19 21:21:40 -0600

    Initial commit of starter code files.

---
+++ utests/utest_Success.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+TEST_CASE( "Simple Success Test" )
+{
+    int i = 42;
+    REQUIRE( i == 42 );
+}
