4eee82acded4490526d7bc2c30d681c056004bb0
Pete Willemsen <willemsn@d.umn.edu>
2026-01-21 12:05:32 -0600

    Added a floating point test in the unit tests.

--- utests/utest_Success.cpp
+++ utests/utest_Success.cpp
@@ -5,4 +5,10 @@ TEST_CASE( "Simple Success Test" )
 {
     int i = 42;
     REQUIRE( i == 42 );
+
+    float myCalcOfPi = 3.141f;
+
+    float eps = 1.0e-3; // a tolerance for how close the answer needs to be
+    float actualAnswer = 3.14159f;
+    REQUIRE_THAT( myCalcOfPi, Catch::Matchers::WithinAbs(actualAnswer, eps));
 }
