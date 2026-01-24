9c52776a6de96a0cf0a51fa0b9fd4b066cccf372
Logan Headrick <headr021@umn.edu>
2026-01-23 20:31:44 -0600

    added to testing vec3

---
+++ src/vec3/READ_ME.txt
@@ -0,0 +1,7 @@
+To run this file, there exists a .exe file that can be ran from the
+green arrow of the IDE (VS comminity 2026 in this case). A terminal 
+window will pop up saying all assertions have passed. 
+
+There is the case that the project hasn't been built locally so there 
+may not be the correct drivers. Build all and it should run without an 
+issue. 
--- utests/utest_vec3.cpp
+++ utests/utest_vec3.cpp
@@ -2,26 +2,62 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <../src/vec3/Vec3.h>
 
-TEST_CASE("Vectors")
+TEST_CASE("Vector Addition")
 {
   vec3 v1 = { 7.0, 14.0, 10.0 };
   vec3 v2 = { 2.0, 2.0, 2.0 };
   vec3 v3 = { 1.0, 1.0, 1.0 };
   vec3 productV;
 
-  productV = operator+(v2, v3);
+  productV = v2+v3;
   REQUIRE(productV == vec3{ 3.0, 3.0, 3.0 }); //Test one, addition
+}
 
-  productV = operator-(v2, v3);
-  REQUIRE(productV == vec3{ 1.0, 1.0, 1.0 }); //Test two, subtraction
+TEST_CASE("Vector Subtraction") {
+  vec3 v1 = { 7.0, 14.0, 10.0 };
+  vec3 v2 = { 2.0, 2.0, 2.0 };
+  vec3 v3 = { 1.0, 1.0, 1.0 };
+  vec3 productV;
 
-  productV = operator*(v2, v3);
-  REQUIRE(productV == vec3{ 2.0, 2.0, 2.0 }); //Test three, multiplicaiton 
+  
+  productV = v2-v3;
+  REQUIRE(productV == vec3{ 1.0, 1.0, 1.0 });// Test two, subtraction
+
+}
+
+TEST_CASE("Vector Multiplicaiton")
+{
+  vec3 v1 = { 7.0, 14.0, 10.0 };
+  vec3 v2 = { 2.0, 2.0, 2.0 };
+  vec3 v3 = { 1.0, 1.0, 1.0 };
+  vec3 productV;
+
+    productV = v2*v3;
+  REQUIRE(productV == vec3{ 2.0, 2.0, 2.0 });// Test three, multiplicaiton
+
+   productV = v2 * v1;
+  REQUIRE(productV == vec3{ 14.0, 28.0, 20.0 });
+}
+
+TEST_CASE("Vector clamp at one")
+{
+  vec3 v1 = { 7.0, 14.0, 10.0 };
+  vec3 v2 = { 2.0, 2.0, 2.0 };
+  vec3 v3 = { 1.0, 1.0, 1.0 };
+  vec3 productV;
+
 
   productV = clampToOne(v1);
-  REQUIRE(productV == v3); //Test 4, clamping to one
+  REQUIRE(productV == v3);// Test 4, clamping to one
+}
+
+TEST_CASE("Vector clamp at n")
+{
+  vec3 v1 = { 7.0, 14.0, 10.0 };
+  vec3 v2 = { 2.0, 2.0, 2.0 };
+  vec3 v3 = { 1.0, 1.0, 1.0 };
+  vec3 productV;
 
    productV = clampToN(v1, 2);
-  REQUIRE(productV == v2); //Test 5, clamping to N
-
+  REQUIRE(productV == v2);// Test 5, clamping to N
 }
