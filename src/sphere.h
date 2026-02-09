9f1d0e37b37fd457eb16ac29f101a22623f42acd
Logan Headrick <headr021@umn.edu>
2026-02-09 16:26:54 -0600

    basic code for shapes and spheres

---
+++ src/sphere.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vec3/vec3.h"
+#include "Shape.h"
+
+class Sphere : public Shape
+{
+public:
+  Sphere() : center(0.0, 0.0, 0.0), radius(1) {}// unit sphere
+
+  // You'll want
+  // Sphere( vec3 c, float r );
+
+  bool intersect(const ray &r, float tmin, float &tmax) override;
+
+private:
+  vec3 center;
+  float radius;
+};
